df24c1cd97813babf23b13771637649b4db25d82
brolyssjlegendario <josequinav78@gmail.com>
2025-06-13 17:53:22 +0200

    Update Config.py

--- Pokedex_flask/Config.py
+++ Pokedex_flask/Config.py
@@ -6,9 +6,11 @@ class Config:  # Define una clase de configuración para la aplicación Flask
     # Establece una clave secreta para la aplicación. 
     # Se usa para la seguridad de sesiones, formularios y autenticación.
 
+    
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'  
     # Define la URL de conexión a la base de datos SQLite.
     # Aquí, 'sqlite:///' indica que se usará SQLite y 'database.db' es el nombre del archivo de la base de datos.
 
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     # Desactiva el seguimiento de modificaciones de objetos en SQLAlchemy para mejorar el rendimiento.
